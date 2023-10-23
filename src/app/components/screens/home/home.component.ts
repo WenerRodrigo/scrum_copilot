7b6070d35a39ee26e0d23418fa269b54f276e163
WenerRodrigo <wener19.wr@gmail.com>
2023-10-23 09:37:46 -0300

    adicionado icon de ocultar senha na tela de login

--- src/app/components/screens/home/home.component.ts
+++ src/app/components/screens/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  password: string = '';
+  visible: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  viewPassword() {
+    this.visible = !this.visible;
   }
 
   singnIn() {
