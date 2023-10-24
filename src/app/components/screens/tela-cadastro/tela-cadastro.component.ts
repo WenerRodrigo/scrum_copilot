f784c6026927c10328eb9d7754a1b0d93f570f9f
mendes033 <joaovitorsousamendes@gmail.com>
2023-10-23 21:33:32 -0300

    Commit chamada api

--- src/app/components/screens/tela-cadastro/tela-cadastro.component.ts
+++ src/app/components/screens/tela-cadastro/tela-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -28,8 +29,32 @@ export class TelaCadastroComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  singIn() {
-    alert("Cadastro realizado com sucesso!");
+  onSubmit() {
+    console.log("this.password")
+    console.log(this.password)
+    // this.singIn(this.password)
   }
 
+  singIn() {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "email": "jao_vitor-sousa@hotmail.com",
+      // "nome_completo": "João Vitor",
+      "senha": "654321",
+      // "data_inclusao": "2023-10-22T17:49:30.159Z"
+    });
+
+    fetch("https://localhost:8000/api/usuarios/authentication", {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
 }
