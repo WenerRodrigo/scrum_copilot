22240e32b4032b796e4090073c01572f569b1c82
WenerRodrigo <wener19.wr@gmail.com>
2023-10-25 18:20:59 -0300

    criando validação com a API

--- src/app/components/screens/reset-senha/reset-senha.component.ts
+++ src/app/components/screens/reset-senha/reset-senha.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-reset-senha',
@@ -10,6 +15,26 @@ export class ResetSenhaComponent implements OnInit {
   password: string = '';
   visible: boolean = false;
   closeVisible: boolean = false;
+  resetSenhaForm: FormGroup;
+
+  constructor() {
+    this.resetSenhaForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+
+    })
+
+  }
+
+  onSubmit() {
+    if (this.resetSenhaForm.valid) {
+      console.log("Senha válida: " + this.resetSenhaForm.value.password)
+    }
+    console.log("this.password")
+    console.log(this.password)
+    //this.singIn(this.password)
+  }
 
   viewPassword() {
     this.visible = !this.visible;
@@ -23,19 +48,29 @@ export class ResetSenhaComponent implements OnInit {
     this.visible = !this.visible;
   }
 
-  constructor() { }
+  singIn() {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "email": "jao_vitor-sousa@hotmail.com",
+      // "nome_completo": "João Vitor",
+      "senha": "654321",
+      // "data_inclusao": "2023-10-22T17:49:30.159Z"
+    });
+
+    fetch("https://localhost:8000/api/usuarios/authentication", {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
 
   ngOnInit(): void {
   }
-
-  onSubmit() {
-    console.log("this.password")
-    console.log(this.password)
-    //this.singIn(this.password)
-  }
-
-  singIn() {
-    alert("Senha alterada com sucesso!")
-  }
-
 }
