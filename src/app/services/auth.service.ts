4f2610a44d4602c3bd5f5514cfc01aaeafdf465c
WenerRodrigo <wener19.wr@gmail.com>
2023-10-25 18:19:05 -0300

    adicionando Url da API

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = "https://localhost:8000/api/usuarios/";
+  constructor(private http: HttpClient) { }
+
+  login(loginObj:any) {
+    return this.http.post<any>(`${this.baseUrl}authentication`, loginObj);
+  }
+
+  singUp(signUpObj:any) {
+    return this.http.post<any>(`${this.baseUrl}gerarusuario`, signUpObj);
+  }
+
+  recuperarEmail(emailObj:any) {
+    return this.http.post<any>(`${this.baseUrl}recuperarsenha_email`, emailObj);
+  }
+
+  validToken(tokenObj:any) {
+    return this.http.post<any>(`${this.baseUrl}token`, tokenObj);
+  }
+
+  resetSenha(resetSenhaObj:any) {
+    return this.http.post<any>(`${this.baseUrl}novasenha`, resetSenhaObj);
+  }
+
+}
