6403ab8d7d33551ffcd570e8231c99855ca0fdb4
WenerRodrigo <wener19.wr@gmail.com>
2023-10-25 18:20:13 -0300

    criando validação de campos

--- src/app/components/screens/home/home.component.ts
+++ src/app/components/screens/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +11,31 @@ export class HomeComponent implements OnInit {
 
   password: string = '';
   visible: boolean = false;
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private auth: AuthService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    } else {
+      console.error('Preencha os campos obrigatórios.');
+    }
   }
 
   viewPassword() {
