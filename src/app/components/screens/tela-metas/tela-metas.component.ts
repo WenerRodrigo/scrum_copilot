648c629cd0447b7af5a6e99339ff9682fb21888a
WenerRodrigo <wener19.wr@gmail.com>
2023-11-12 16:45:17 -0300

    add barra de pesquisa na tela de gerenciamento de meteas

--- src/app/components/screens/tela-metas/tela-metas.component.ts
+++ src/app/components/screens/tela-metas/tela-metas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Data } from '@angular/router';
+import { EndPointService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-tela-metas',
@@ -7,11 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TelaMetasComponent implements OnInit {
+  meuFormulario!: FormGroup;
   metas: Meta[] = [];
   searchText: string = '';
-  metaEmEdicao: Meta | null = null;
+  metaEmEdicao: string | null = null;
+  isPopupVisible: boolean = false;
+  originalMetas: any[] = [];
 
-  constructor() {
+  constructor( private authService: EndPointService) {
     this.metas = [
       {
         nomeMeta: 'Meta 1',
@@ -27,7 +33,7 @@ export class TelaMetasComponent implements OnInit {
         dataInicio: new Date('2021-02-01'),
         dataFim: new Date('2021-02-28'),
         responsavel: 'Maria',
-        status: 'Em andamento'
+        status: 'Concluido'
       },
       {
         nomeMeta: 'Meta 3',
@@ -40,21 +46,84 @@ export class TelaMetasComponent implements OnInit {
     ];
   }
 
-  // applyFilter() {
-  //   if (this.searchText) {
-  //     this.metas = this.metas.filter(meta =>
-  //       meta.nomeMeta.toLowerCase().includes(this.searchText.toLowerCase()) ||
-  //       meta.meta.toLowerCase().includes(this.searchText.toLowerCase())
-  //     );
-  //   } else {
-  //     this.metas = [];
-  //   }
-  // }
+  openPopup() {
+    this.isPopupVisible = true;
+  }
+
+  closePopup() {
+    this.isPopupVisible = false;
+  }
+
+  submitForm() {
+    const data: Data = ({
+      titulo_meta: this.meuFormulario.value.titulo_meta,
+      descricao: this.meuFormulario.value.descricao,
+      data_inicio: this.meuFormulario.value.data_inicio,
+      data_conclusao_prevista: this.meuFormulario.value.data_conclusao_prevista
+    })
+    console.log(data)
+
+    if (this.metaEmEdicao !== null) {
+      this.authService.atualizarMeta(this.metaEmEdicao, data).subscribe({
+        next: (response) => {
+          if (response.response === 200) {
+            console.log('Cadastro realizado com sucesso');
+            this.closePopup();
+            window.location.reload();
+          }
+          else {
+            console.log(response)
+            alert(response.mensagem)
+          }
+        },
+        error: (error) => {
+          console.error('Erro ao cadastrar:', error);
+        }
+      });
+    }
+    else if (this.metaEmEdicao === null && this.meuFormulario.valid) {
+
+      this.authService.criarMeta(data).subscribe({
+        next: (response) => {
+          if (response.response === 200) {
+            console.log('Cadastro realizado com sucesso');
+            this.closePopup();
+            window.location.reload();
+          }
+          else {
+            console.log(response)
+            alert(response.mensagem)
+          }
+        },
+        error: (error) => {
+          console.error('Erro ao cadastrar:', error);
+        }
+      });
+      const formValues = this.meuFormulario.value;
+      this.metas.push(formValues);
+    }
+    else {
+      console.log(data)
+    }
+  }
+
+
+  applyFilter() {
+    if (this.searchText.trim() !== '') {
+      this.metas = this.originalMetas.filter(meta =>
+        meta.titulo_meta.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        meta.descricao.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    } else {
+      this.metas = [...this.originalMetas];
+    }
+  }
+
 
   ngOnInit(): void { }
 
   editar(meta: Meta) {
-    this.metaEmEdicao = meta;
+    this.metaEmEdicao = meta.nomeMeta;
   }
 
   remover(index: number) {
